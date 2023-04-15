3c70fb5756b8073430e79749af76ad149fe2e8ed
Gorro_Rojo <gorro.rojo@gmail.com>
2023-04-14 22:39:11 -0300

    Added head title for markdown pages

--- src/routes/[slug]/+page.svelte
+++ src/routes/[slug]/+page.svelte
@@ -2,7 +2,10 @@
 	//@ts-nocheck
 	export let data;
 </script>
-
+<svelte:head>
+    <title>KinkyVibe - {data.title}</title>
+    <meta property="og:title" content={data.title} />
+</svelte:head>
 <h1>{data.title}</h1>
 <p>Published: {data.date}</p>
 <svelte:component this={data.content} />
