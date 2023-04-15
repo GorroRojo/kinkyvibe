cdb969c8b911f5643697f43ef41280fc4f5c7aef
Gorro_Rojo <gorro.rojo@gmail.com>
2023-04-14 22:42:07 -0300

    :ambulance: Disabled prerender and enabled csr to unblock build

--- src/routes/+layout.js
+++ src/routes/+layout.js
@@ -1,5 +1,6 @@
-export const prerender = true;
+// export const prerender = true;
+// export const csr = false;
+// TODO fix prerender and csr blocking the build
 export const load = ({ url }) => {
 	return { currentRoute: url.pathname };
 };
-// export const csr = false;
