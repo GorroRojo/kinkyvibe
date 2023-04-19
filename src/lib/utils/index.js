f61587ea7c13be95acec621e88c67e923aeae311
Gorro_Rojo <gorro.rojo@gmail.com>
2023-04-19 19:09:38 -0300

    Trying to fix error on deploy

--- src/lib/utils/index.js
+++ src/lib/utils/index.js
@@ -1,6 +1,6 @@
 //@ts-nocheck
 export const fetchMarkdownPosts = async () => {
-	const allPostFiles = import.meta.glob('/src/lib/posts/*.md');
+	const allPostFiles = import.meta.glob('$lib/posts/*.md');
 	const iterablePostFiles = Object.entries(allPostFiles);
 	const allPosts = await Promise.all(
 		iterablePostFiles.map(async ([path, resolver]) => {
