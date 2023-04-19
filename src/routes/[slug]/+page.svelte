f8644b24b2cb1bdf9f47be53b913fa9fc8790c23
Gorro_Rojo <gorro.rojo@gmail.com>
2023-04-19 18:48:38 -0300

    Handled errors

--- src/routes/[slug]/+page.svelte
+++ src/routes/[slug]/+page.svelte
@@ -2,10 +2,15 @@
 	//@ts-nocheck
 	export let data;
 </script>
+
 <svelte:head>
-    <title>KinkyVibe - {data.title}</title>
-    <meta property="og:title" content={data.title} />
+	<title>KinkyVibe - {data.title}</title>
+	<meta property="og:title" content={data.title} />
 </svelte:head>
 <h1>{data.title}</h1>
-<p>Published: {data.date}</p>
-<svelte:component this={data.content} />
+{#if !data.error}
+	<!-- <p>Published: {new Date(data.date)}</p> -->
+	<svelte:component this={data.content} />
+{:else}
+	{data.content}
+{/if}
