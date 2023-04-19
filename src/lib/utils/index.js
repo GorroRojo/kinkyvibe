335c644c43450d1682773d18162b6ff5e886d2f3
Gorro_Rojo <gorro.rojo@gmail.com>
2023-04-19 18:56:59 -0300

    Fixed more issues with repositioning posts

--- src/lib/utils/index.js
+++ src/lib/utils/index.js
@@ -1,11 +1,11 @@
 //@ts-nocheck
 export const fetchMarkdownPosts = async () => {
-	const allPostFiles = import.meta.glob('/src/routes/[slug]/posts/*.md');
+	const allPostFiles = import.meta.glob('/src/lib/posts/*.md');
 	const iterablePostFiles = Object.entries(allPostFiles);
 	const allPosts = await Promise.all(
 		iterablePostFiles.map(async ([path, resolver]) => {
 			const { metadata } = await resolver();
-			const postPath = path.slice(25, -3);
+			const postPath = path.slice(15, -3);
 			return { meta: metadata, path: postPath };
 		})
 	);
