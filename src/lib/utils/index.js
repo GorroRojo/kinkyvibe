13d444e937f706811578c3fd8269e08f0e882f1b
Gorro_Rojo <gorro.rojo@gmail.com>
2023-04-19 18:48:33 -0300

    Updated code for moved pages

--- src/lib/utils/index.js
+++ src/lib/utils/index.js
@@ -1,12 +1,11 @@
 //@ts-nocheck
 export const fetchMarkdownPosts = async () => {
-	const allPostFiles = import.meta.glob('/src/routes/[slug]/*.md');
+	const allPostFiles = import.meta.glob('/src/routes/[slug]/posts/*.md');
 	const iterablePostFiles = Object.entries(allPostFiles);
-
 	const allPosts = await Promise.all(
 		iterablePostFiles.map(async ([path, resolver]) => {
 			const { metadata } = await resolver();
-			const postPath = path.slice(18, -3);
+			const postPath = path.slice(25, -3);
 			return { meta: metadata, path: postPath };
 		})
 	);
--- src/routes/[slug]/+page.js
+++ src/routes/[slug]/+page.js
@@ -1,7 +1,12 @@
+//@ts-nocheck
 export async function load({ params }) {
-	const post = await import(`./posts/${params.slug}.md`);
-	const { title, date } = post.metadata;
-	const content = post.default;
+	try {
+		const post = await import(`/src/lib/posts/${params.slug}.md`);
+		const { title, date } = post.metadata;
+		const content = post.default;
 
-	return { content, title, date };
+		return { content, title, date };
+	} catch (e) {
+		return { content: e + '\nparams.slug: ' + params.slug, title: 'Error', date: '', error: true };
+	}
 }
