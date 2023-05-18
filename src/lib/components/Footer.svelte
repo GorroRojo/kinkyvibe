0bfe559cb99af2824545ca79dcc0fc39a1505479
Gorro_Rojo <gorro.rojo@gmail.com>
2023-05-18 07:54:04 -0300

    New Footer component

---
+++ src/lib/components/Footer.svelte
@@ -0,0 +1,58 @@
+<footer>
+	Codigueado por <a href="https://www.gorro.ar/">Gorro_Rojo</a>
+	<ul>
+		<li>
+			<h3>Contenido</h3>
+			<ul>
+				<li><a href="/material">Material</a></li>
+				<li><a href="/amigues">Amigues</a></li>
+				<li><a href="/calendario">Calendario</a></li>
+				<li><a href="https://kinkyvibe.mitiendanube.com">Tienda</a></li>
+			</ul>
+		</li>
+		<li>
+			<h3>Redes</h3>
+			<ul>
+				<li><a href="https://instagram.com/kinkyvibeargentina">Instagram</a></li>
+				<li><a href="/">Twitter</a></li>
+				<li><a href="/">TikTok</a></li>
+				<li><a href="/">Youtube</a></li>
+			</ul>
+		</li>
+	</ul>
+</footer>
+
+<style>
+	footer {
+		margin: 0;
+		margin-top: 8em;
+		padding: 2em;
+		background: indigo;
+		color: white;
+		border-radius: 2em 2em 0 0;
+		position: absolute;
+		bottom: 0;
+		left: 0;
+		right: 0;
+		box-sizing: border-box;
+		/* transition: 1000ms; */
+	}
+	a {
+		color: white;
+		text-decoration: underline;
+		text-decoration-color: rgba(250,250,250,.4);
+	}
+	a:hover {
+		text-decoration-color: var(--1);
+	}
+	footer > ul {
+		display: flex;
+		list-style: none;
+		justify-content: space-around;
+		padding: 0;
+	}
+	li > ul {
+		padding: 0;
+		list-style: none;
+	}
+</style>
