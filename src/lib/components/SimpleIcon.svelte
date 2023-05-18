03b72118205e09bc725af6bab6364a44db315404
Gorro_Rojo <gorro.rojo@gmail.com>
2023-05-18 07:51:34 -0300

    Added better SimpleIcon support

---
+++ src/lib/components/SimpleIcon.svelte
@@ -0,0 +1,30 @@
+<script>
+    
+    // @ts-nocheck
+    /** Icon data from SimpleIcon's module import
+     * @type {{
+     *      title: string,
+     *      slug: string,
+     *      hex: string,
+     *      source: string,
+     *      svg: string,
+     *      path: string,
+     *      guidelines: string,
+     *      license: {
+     *          type:string,
+     *          url: string
+     *      }
+     * }}
+     */
+    export let icon;
+</script>
+<svg role="img" viewbox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+    {@html icon.svg.slice(71,-5)}
+</svg>
+<style>
+    svg {
+        color: var(--color, var(--1));
+        fill: var(--color, var(--1));
+        /* stroke: var(--color, var(--1)); */
+    }
+</style>
