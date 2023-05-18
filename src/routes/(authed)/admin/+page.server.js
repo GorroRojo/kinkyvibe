8f5157f89cde91a1b49cbf93bd2eba7a840634ab
Gorro_Rojo <gorro.rojo@gmail.com>
2023-05-18 07:55:44 -0300

    Added some error handling in admin panel

--- src/routes/(authed)/admin/+page.server.js
+++ src/routes/(authed)/admin/+page.server.js
@@ -1,22 +1,22 @@
 //@ts-nocheck
-import { env } from '$env/dynamic/private';
-import { Octokit } from '@octokit/core';
-import { error } from '@sveltejs/kit';
+// import { env } from '$env/dynamic/private';
+// import { Octokit } from '@octokit/core';
+// import { error } from '@sveltejs/kit';
 export async function load() {
-	const octokit = new Octokit({ auth: env.GITHUB_TOKEN });
-	let existing;
-	try {
-		existing = await octokit.request('GET /repos/{owner}/{repo}/contents/{path}', {
-			owner: 'GorroRojo',
-			repo: 'KinkyVibe',
-			path: 'src/lib/posts/nuevo.md',
-			headers: {
-				'X-GitHub-Api-Version': '2022-11-28'
-			}
-		});
-	} catch (e) {
-		error(e);
-	}
+	// const octokit = new Octokit({ auth: env.GITHUB_TOKEN });
+	let existing=false;
+	// try {
+		// existing = await octokit.request('GET /repos/{owner}/{repo}/contents/{path}', {
+		// 	owner: 'GorroRojo',
+		// 	repo: 'KinkyVibe',
+		// 	path: 'src/lib/posts/nuevo.md',
+		// 	headers: {
+		// 		'X-GitHub-Api-Version': '2022-11-28'
+		// 	}
+		// });
+	// } catch (e) {
+		// throw error(400,e);
+	// }
     let gitHubRequestOptions = {
 		owner: 'GorroRojo',
 		repo: 'KinkyVibe',
@@ -27,12 +27,12 @@ export async function load() {
 			'X-GitHub-Api-Version': '2022-11-28'
 		}
 	}
-    if (existing.status != 404) {
+    if (existing && existing?.status != 404) {
         gitHubRequestOptions.sha = existing.data.sha;
     }
 	// let i;
 	// i = await octokit.request('PUT /repos/{owner}/{repo}/contents/{path}', gitHubRequestOptions);
 	return {
-		res: existing.data.content
+		res: existing?.data?.content
 	};
 }
--- src/routes/(authed)/admin/+page.svelte
+++ src/routes/(authed)/admin/+page.svelte
@@ -1,7 +1,7 @@
 <script>
     //@ts-nocheck
     export let data;
-    let c = atob(data.res,'base64')
+    let c = atob(data.res??'','base64')
     function save(e) {
         alert(document.querySelector("textarea").value)
     }
@@ -9,5 +9,5 @@
 <textarea value={c}></textarea>
 <button on:click={save}>Save</button>
 <form method="POST" action="/logout">
-    <input type="submit" value="Log out">
+    <input type="submit" value="Log out"/>
  </form>
