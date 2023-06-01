95d056757eb039f6b2d825481c91ad51b673a6ca
Gorro_Rojo <gorro.rojo@gmail.com>
2023-06-01 15:46:04 -0300

    scss :global hotfix

--- src/lib/mdsvex/layout.svelte
+++ src/lib/mdsvex/layout.svelte
@@ -1,27 +1,27 @@
 <script>
-    export let title = ''
+	export let title = '';
 </script>
 
 <h1>{title}</h1>
-<slot/>
+<slot />
+
 <style lang="scss">
-    h1 {
+	h1 {
 		font-size: 2em;
 	}
-    :global(article *) {
-        font-size: 1.5rem;
-    }
-    :global(strong) {
-        color: var(--2);
-        font-weight: black;
-    }
+	:global(article *) {
+		font-size: 1.5rem;
+	}
+	:global(strong) {
+		color: var(--2);
+		font-weight: black;
+	}
 	:global(img) {
 		margin-inline: auto;
 		position: relative;
 		width: 100%;
 		object-fit: contain;
-        border-radius: 1em;
-
+		border-radius: 1em;
 	}
 	:global(pre) {
 		padding: 1em;
@@ -32,39 +32,37 @@
 		padding: 0.1em 0.4em;
 		border-radius: 0.4em;
 		font-family: 'Courier New', Courier, monospace !important;
-		font-size: .9em;
-		span.token  {
-			font-family: 'Courier New', Courier, monospace !important;
-
-		}
+		font-size: 0.9em;
+	}
+	:global(code span.token) {
+		font-family: 'Courier New', Courier, monospace !important;
 	}
 	:global(code, pre) {
 		background: var(--3);
 	}
-    :global(.col-2),
-    :global(.col-3),
-    :global(.col-4) {
-        display: grid;
-        gap: .5em;
-        margin-block: 1em;
-        margin-inline: -4em;
-        max-width: 100vw;
-    }
-    @media (max-width: 900px) {
-        :global(.col-2),
-    :global(.col-3),
-    :global(.col-4) {
-        margin-inline: 0;
-    }
-    }
-    :global(.col-2) {
-        grid-template-columns: 1fr 1fr;
-    }
-    :global(.col-3) {
-        grid-template-columns: 1fr 1fr 1fr;
-    }
-    :global(.col-4) {
-        grid-template-columns: 1fr 1fr 1fr 1fr;
-    }
-    
+	:global(.col-2),
+	:global(.col-3),
+	:global(.col-4) {
+		display: grid;
+		gap: 0.5em;
+		margin-block: 1em;
+		margin-inline: -4em;
+		max-width: 100vw;
+	}
+	@media (max-width: 900px) {
+		:global(.col-2),
+		:global(.col-3),
+		:global(.col-4) {
+			margin-inline: 0;
+		}
+	}
+	:global(.col-2) {
+		grid-template-columns: 1fr 1fr;
+	}
+	:global(.col-3) {
+		grid-template-columns: 1fr 1fr 1fr;
+	}
+	:global(.col-4) {
+		grid-template-columns: 1fr 1fr 1fr 1fr;
+	}
 </style>
