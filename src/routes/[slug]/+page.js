ac2f5518c737e9f41cf880249d32ea25a35884da
Gorro_Rojo <gorro.rojo@gmail.com>
2023-06-11 02:27:46 -0300

    add head metadata on posts

--- src/routes/[slug]/+page.js
+++ src/routes/[slug]/+page.js
@@ -3,10 +3,9 @@ export const prerender = 'auto';
 export async function load({ params }) {
 	try {
 		const post = await import(`../../lib/posts/${params.slug}.md`);
-		const { title, date } = post.metadata;
 		const content = post.default;
 
-		return { content, title, date };
+		return { content, ...post.metadata };
 	} catch (e) {
 		return { content: e + '\nparams.slug: ' + params.slug, title: 'Error', date: '', error: true };
 	}
