5974dd6695d6c036c127f8d4038d783883093bdb
Gorro_Rojo <gorro.rojo@gmail.com>
2023-07-24 19:09:51 -0300

    fix inlinetag not updating

--- src/routes/(content)/material/InlineTag.svelte
+++ src/routes/(content)/material/InlineTag.svelte
@@ -1,6 +1,6 @@
 <script>
 	import { page } from '$app/stores';
-	import { alias, togglePositiveTagFilterFn } from '$lib/utils/stores';
+	import { alias, togglePositiveTagFilterFn, filteredTags } from '$lib/utils/stores';
 	import Tag from '$lib/components/Tag.svelte';
 
 	/** @type string */
@@ -9,21 +9,18 @@
 	export let internalTag;
 	/** @type {(a:string,b:string)=>*}*/
 	let toggle = $togglePositiveTagFilterFn;
-	
-	togglePositiveTagFilterFn.subscribe(f=>toggle = f);
-	
+	let checked = $filteredTags.includes(tag);
 	const aliasedTag = $alias(internalTag ?? tag);
+
+	togglePositiveTagFilterFn.subscribe((f) => (toggle = f));
+	filteredTags.subscribe((f) => (checked = f.includes(aliasedTag ?? internalTag ?? tag)));
 </script>
 
 <Tag
 	onInput={(/**@type {*} */ evt) => toggle(evt.target?.checked, aliasedTag ?? internalTag ?? tag)}
 	{tag}
 	isCheckbox
-	checked={$page.url.searchParams.has('tags') &&
-		$page.url.searchParams
-			.get('tags')
-			?.split(',')
-			.includes(aliasedTag ?? internalTag ?? tag)}
+	{checked}
 	--off-background="color-mix(in srgb, var(--1-light) 10%, transparent)"
 	--font-size="1em"
 	--padding="0.1em 0.2em"
