1bd925d341422686563da17aaab2e84c2dd88da9
Gorro_Rojo <gorro.rojo@gmail.com>
2023-08-07 00:55:30 -0300

    pin website hosts in amigues

--- src/routes/(content)/amigues/+page.svelte
+++ src/routes/(content)/amigues/+page.svelte
@@ -3,7 +3,14 @@
 	import { ArrowLeft } from 'lucide-svelte';
 	export let data;
 	import PostList from '$lib/components/PostList.svelte';
+	let pinned = ['Gorro_Rojo', 'DemonWeb', 'TallarinesConTuco']
+	data.posts.sort((a, b) => {
+		if (pinned.includes(a.path) && !pinned.includes(b.path)) {
+			return -1;
+		} else return 0;
+	});
 </script>
+
 <svelte:head>
 	<title>KinkyVibe.ar - Emprendimientos y profesionales</title>
 </svelte:head>
