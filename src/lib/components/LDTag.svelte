14265f09a9594e75c98b33753ebf16420a1ccb06
Gorro_Rojo <gorro.rojo@gmail.com>
2023-09-01 16:43:45 -0300

    create JSON+LD component

---
+++ src/lib/components/LDTag.svelte
@@ -0,0 +1,65 @@
+<script context="module">
+    import '$lib/types.d.js';
+	let site = {
+		title: 'Navillus: Jamstack developers for hire',
+		description:
+			"We specialize in finding simple solutions to complex software problems. We value data-driven decisions and accessibility over this month's latest tech trends.",
+		url: 'https://KinkyVibe.ar',
+		author: 'KinkyVibe',
+		social: {
+			twitter: 'navillus_dev',
+			twitter_card: 'summary_large_image'
+		},
+		company: {
+			name: 'Navillus'
+		}
+	};
+    /**@type import('schema-dts').WithContext<import('schema-dts').Organization>*/
+     const organizationSchema = {
+        "@context": "https://schema.org",
+        "@type": "Organization",
+        "@id": `${site.url}#organization`,
+        url: site.url,
+        name: site.company.name,
+        description: site.description,
+        sameAs: [`https://twitter.com/${site.social.twitter}`],
+        logo: `${site.url}/favicon.svg`,
+    };
+
+     const websiteSchema = {
+		'@context': 'https://schema.org',
+		'@type': 'WebSite',
+		name: site.title,
+		url: site.url,
+		description: site.description,
+		sameAs: [`https://twitter.com/${site.social.twitter}`]
+	};
+	/**
+	 * @param {LD.Schema} thing
+	 **/
+	function serializeSchema(thing) {
+		return '<script type="application/ld+json">' + JSON.stringify(thing, null, 2) + '<\/script>';
+	}
+
+</script>
+<script>
+    /**@type LD.Schema*/
+    export let schema;
+
+</script>
+<svelte:head>
+	{@html serializeSchema(schema)}
+</svelte:head>
+<!-- {
+    "title": "Navillus: Jamstack developers for hire",
+    "description": "We specialize in finding simple solutions to complex software problems. We value data-driven decisions and accessibility over this month's latest tech trends.",
+    "url": "https://navillus.dev",
+    "author": "Navillus",
+    "social": {
+        "twitter": "navillus_dev",
+        "twitter_card": "summary_large_image"
+    },
+    "company": {
+        "name": "Navillus"
+    }
+} -->
--- src/lib/types.d.js
+++ src/lib/types.d.js
@@ -1,6 +1,6 @@
 /**
  * @typedef { 'head' |'face' |'neck' |'shoulder-left' |'shoulder-right' |'arm-left' |'forearm-left' |'arm-right' |'forearm-right' |'chest-left' |'chest-right' |'belly-left' |'ribs-left' |'belly-right' |'belly' |'ribs-right' |'thigh-left' |'innerthigh-left' |'feet-left' |'calf-left' |'knee-left' |'thigh-right' |'genitalia' |'innerthigh-right' |'right-feet' |'calf-right' |'knee-right' |'elbow-right' |'hand-right' |'elbow-left' |'hands-left' |'armback-left' |'leg-left' |'buttock' |'loin' |'column' |'head-back' |'nape' |'armback-right' |'leg-right' |'back-right' |'clavicule-right' |'back-left' |'clavicule-left'|string  } BodyPart
-*/
+ */
 /**@typedef Group
  * @prop {string} name
  * @prop {string[]} members
@@ -39,12 +39,12 @@
  * 		link: string,
  * 		access_date: Date,
  * 		original_published_date: Date
- *      redirect: boolean 
+ *      redirect: boolean
  *      wiki?: string
  * }} MaterialPostData
  */
 /** @typedef {PostData & {
- *		status: 'abierto' | 'anunciado' | 'lleno',
+ *		status: 'abierto' | 'anunciado' | 'lleno' | 'cancelado',
  * 		start: string,
  * 		end?: string,
  * 		duration?: Date,
@@ -67,3 +67,21 @@
  * }} AmiguesPostData */
 // TODO affiliation, education, experience, skill
 /** @typedef {AmiguesPostData & MaterialPostData & CalendarioPostData} AnyPostData */
+
+/**
+ * @typedef {import('schema-dts').Thing | import('schema-dts').WithContext<import('schema-dts').Thing>} LD.Schema
+ */
+/** @typedef {import('schema-dts').Organization} LD.Organization */
+/** @typedef {import('schema-dts').WebSite} LD.Website */
+/** @typedef {import('schema-dts').Article} LD.Article */
+/** @typedef {import('schema-dts').Event} LD.Event */
+/** @typedef {import('schema-dts').EventSeries} LD.EventSeries */
+/** @typedef {import('schema-dts').Person} LD.Person */
+/** @typedef {import('schema-dts').ProfilePage} LD.ProfilePage */
+/** @typedef {import('schema-dts').EventVenue} LD.EventVenue */
+/** @typedef {import('schema-dts').Book} LD.Book */
+/** @typedef {import('schema-dts').BreadcrumbList} LD.BreadcrumbList */
+/** @typedef {import('schema-dts').ClaimReview} LD.ClaimReview */
+/** @typedef {import('schema-dts').FAQPage} LD.FAWPage */
+/** @typedef {import('schema-dts').SpeakableSpecification} LD.SpeakableSpecification */
+/** @typedef {import('schema-dts').SearchAction} LD.SearchAction */
