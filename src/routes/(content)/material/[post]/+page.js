68fe1417e068be2c72481cf10df01333e59861e8
Gorro_Rojo <gorro.rojo@gmail.com>
2023-09-13 23:32:40 -0300

    reimplement redirect for posts

--- src/routes/(content)/material/[post]/+page.js
+++ src/routes/(content)/material/[post]/+page.js
@@ -1,6 +1,12 @@
 import { fetchPost } from '$lib/utils';
+import { redirect } from '@sveltejs/kit';
 
 /** @type {import("./$types").PageLoad} */
 export async function load({ params }) {
-	return await fetchPost('material', params.post);
+	let post = await fetchPost('material', params.post);
+	if (post.meta?.redirect) {
+		throw redirect(307, post.meta.link);
+	}
+	return post;
 }
+
