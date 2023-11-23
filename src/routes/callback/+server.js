8a5254658e798cddbfa9afb4585b6c54370e51ef
Gorro_Rojo <gorro.rojo@gmail.com>
2023-11-22 22:05:16 -0300

    cleanup and handle errors

--- src/routes/callback/+server.js
+++ src/routes/callback/+server.js
@@ -7,50 +7,76 @@ const userURL = 'https://api.github.com/user';
 export async function GET(request) {
 	// get accessToken
 	const code = request.url.searchParams.get('code') ?? '';
-    console.log("getting token from GH using code: " + code)
+	console.log('getting token from GH using code: ' + code);
 	const token = await getToken(code);
-    console.log("getting user from GH using token: " + token)
+	console.log('getting user from GH using token: ' + token);
 	const user = await getUser(token);
-    // @ts-ignore
-    request.locals.user = user.login
+	// @ts-ignore
+	request.locals.user = user.login;
 	// return json(user);
-    throw redirect(302,'/admin');
+	throw redirect(302, '/admin');
+}
+
+/**
+ *
+ *
+ * @param {string} code
+ * @return {string}
+ */
+function getToken(code) {
+	// @ts-ignore
+	return fetch(tokenURL, {
+		method: 'POST',
+		headers: {
+			'Content-Type': 'application/json',
+			Accept: 'application/json'
+		},
+		body: JSON.stringify({
+			client_id: clientId,
+			client_secret: secret,
+			code: code
+		})
+	})
+		.then((r) => r.json())
+		.then((r) => {
+			if (r.error) {
+				throw new Error(
+					'Error from GitHub API: \n' + r.error + '\n' + r.error_description + '\n' + r.error_uri
+				);
+			} else {
+				return r.access_token;
+			}
+		})
+		.catch((err) => {
+			throw new Error('Error at getToken: ' + err);
+		});
 }
 /**
- * @typedef {Object} GHUser
- * @property {string} login
- * @property {number} id
- * @property {string} node_id
- * @property {string} avatar_url
- * @property {string} gravatar_id
- * @property {string} url
- * @property {string} html_url
- * @property {string} followers_url
- * @property {string} following_url
- * @property {string} gists_url
- * @property {string} starred_url
- * @property {string} subscriptions_url
- * @property {string} organizations_url
- * @property {string} repos_url
- * @property {string} events_url
- * @property {string} received_events_url
- * @property {string} type
- * @property {boolean} site_admin
- * @property {string} name
- * @property {null|string} company
- * @property {string} blog
- * @property {string} location
- * @property {string} email
- * @property {boolean} hireable
- * @property {null|string} bio
- * @property {null|string} twitter_username
- * @property {number} public_repos
- * @property {number} public_gists
- * @property {number} followers
- * @property {number} following
- * @property {string} created_at
- * @property {string} updated_at
+ *
+ *
+ * @param {string} token
+ * @return {GHUser}
  */
+function getUser(token) {
+	// @ts-ignore
+	return fetch(userURL, {
+		headers: {
+			Accept: 'application/json',
+			Authorization: `Bearer ${token}`
+		}
+	})
+		.then((r) => {
+			try {
+				return r.json();
+			} catch (e) {
+				throw new Error('JSON Error for response: \n' + r.status + '\n' + r.statusText);
+			}
+		})
+		.catch((err) => {
+			throw new Error('Error at getUser: ' + err);
+		});
+}
+
 // let i = {
 // 	login: 'GorroRojo',
 // 	id: 4594048,
@@ -67,50 +93,3 @@ export async function GET(request) {
 // 	blog: 'https://gorro.ar',
 // 	email: 'gorro.rojo@gmail.com',
 // };
-/**
- *
- *
- * @param {string} code
- * @return {string} 
- */
-function getToken(code) {
-	// @ts-ignore
-	return fetch(tokenURL, {
-		method: 'POST',
-		headers: {
-			'Content-Type': 'application/json',
-			Accept: 'application/json'
-		},
-		body: JSON.stringify({
-			client_id: clientId,
-			client_secret: secret,
-			code: code
-		})
-	})
-		.then((r) => {
-            console.log(r)
-            return r.json()
-        })
-		.then((r) => {
-            console.log(r)
-            return r.access_token
-        })
-        .catch((err)=> {throw new Error("Error at getToken: "+err)});
-}
-/**
- *
- *
- * @param {string} token
- * @return {GHUser} 
- */
-function getUser(token) {
-	// @ts-ignore
-	return fetch(userURL, {
-		headers: {
-			Accept: 'application/json',
-			Authorization: `Bearer ${token}`
-		}
-	}).then((r) => r.json())
-    .catch((err)=> {throw new Error("Error at getUser: "+err)});
-
-}
