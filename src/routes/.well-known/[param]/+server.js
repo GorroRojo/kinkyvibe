a26d8abb5271a1a2d8ffe66d526b6a37d5ae2f0d
Gorro_Rojo <gorro.rojo@gmail.com>
2024-01-24 19:08:27 -0300

    fix redirect returning instead of throwing

--- src/routes/.well-known/[param]/+server.js
+++ src/routes/.well-known/[param]/+server.js
@@ -3,5 +3,5 @@ import { redirect } from "@sveltejs/kit";
 /** @type {import("./$types").RequestHandler} */
 export function GET({ url }) {
     url.host = "fed.brid.gy"
-    return redirect(303, url);
+    throw redirect(303, url);
 }
