2b551d408dbccdf28878f148a8a63a84d97834a3
Gorro_Rojo <gorro.rojo@gmail.com>
2024-01-24 18:17:19 -0300

    enhance homepage h-card

--- src/routes/(content)/+page.svelte
+++ src/routes/(content)/+page.svelte
@@ -57,7 +57,7 @@
 <LDTag schema={websiteSchema} />
 {#await kinkyProfile then profile}
 	<div hidden class="profile-header h-card p-contact">
-		<a class="u-url u-uid" href="https://kinkyvibe.ar">https://kinkyvibe.ar</a> 
+		<a class="u-url u-uid" href="https://kinkyvibe.ar/">https://kinkyvibe.ar/</a> 
 		<img class="profile-pic u-photo" src={profile.meta.featured + ''} alt="" />
 		<h1 id="title" class="profile-name p-name">
 			{profile.meta.title}
