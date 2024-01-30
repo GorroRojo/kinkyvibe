66f836cc8d111365d2d09b96675c2ab0cd9e0959
Gorro_Rojo <gorro.rojo@gmail.com>
2024-01-29 22:41:41 -0300

    make api prerenderable

--- src/routes/api/posts/+server.js
+++ src/routes/api/posts/+server.js
@@ -1,5 +1,8 @@
 import { json } from '@sveltejs/kit';
 import { fetchMarkdownPosts } from '$lib/utils';
+export const prerender = true;
+
+/** @type {import("../$types").RequestHandler} */
 export async function GET() {
 	/**@type ProcessedPost[] */
 	return json(await fetchMarkdownPosts());
--- src/routes/api/wiki/+server.js
+++ src/routes/api/wiki/+server.js
@@ -1,6 +1,9 @@
 import { json } from '@sveltejs/kit';
 import { fetchMarkdownPosts } from '$lib/utils';
+export const prerender = true;
+
+/** @type {import("../$types").RequestHandler} */
 export async function GET() {
 	/**@type ProcessedPost[] */
-	return json(await fetchMarkdownPosts());
+	return json(await fetchMarkdownPosts(true));
 }
