2880eb718ed6c9f65cace2c5a4fbf529c262c0df
Gorro_Rojo <gorro.rojo@gmail.com>
2024-01-29 22:03:35 -0300

    log list fetching time

--- src/routes/+layout.server.js
+++ src/routes/+layout.server.js
@@ -1,10 +1,20 @@
 import { fetchMarkdownPosts } from '$lib/utils';
 /** @type {import("./$types").LayoutServerLoad} */
-export const load = async ({ url, locals }) => {
+export const load = async ({fetch, url, locals }) => {
+	let t1 = Date.now();
+	let allPosts = await fetchMarkdownPosts();
+	let wiki = await fetchMarkdownPosts(true);
+	let t2 = Date.now();
+	console.log('calling time:', t2 - t1);
+	// t1 = Date.now();
+	// allPosts = await (await fetch('/api')).json()
+	// wiki = await (await fetch('/api?wiki')).json();
+	// t2 = Date.now();
+	// console.log('fetching time:', t2 - t1);
 	return {
 		currentRoute: url.pathname,
-		allPosts: await fetchMarkdownPosts(),
-		wiki: await fetchMarkdownPosts(true),
+		allPosts,
+		wiki,
 		user: locals.user
 	};
 };
