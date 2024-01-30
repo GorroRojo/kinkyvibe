48e3c72587f615e16031500b43b528a4e83d4b47
Gorro_Rojo <gorro.rojo@gmail.com>
2024-01-30 00:26:17 -0300

    fix strange header-changing server error maybe

--- src/routes/+layout.server.js
+++ src/routes/+layout.server.js
@@ -1,10 +1,10 @@
-// import { fetchMarkdownPosts } from '$lib/utils';
+import { fetchMarkdownPosts } from '$lib/utils';
 /** @type {import("./$types").LayoutServerLoad} */
 export const load = async ({fetch, url, locals }) => {
-	// let allPosts = await fetchMarkdownPosts();
-	// let wiki = await fetchMarkdownPosts(true);
-	let allPosts = await (await fetch('/api/posts')).json()
-	let wiki = await (await fetch('/api/wiki')).json();
+	let allPosts = await fetchMarkdownPosts();
+	let wiki = await fetchMarkdownPosts(true);
+	// let allPosts = await (await fetch('/api/posts')).json()
+	// let wiki = await (await fetch('/api/wiki')).json();
 	return {
 		currentRoute: url.pathname,
 		allPosts,
