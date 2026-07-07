d034ebc41ce0b5146230d282dae140fd0f1b9fd0
Gorro_Rojo <gorro.rojo@gmail.com>
2026-07-07 16:31:44 -0300

    allow redirect posts on calendario

--- src/routes/(content)/calendario/[event]/+page.js
+++ src/routes/(content)/calendario/[event]/+page.js
@@ -1,6 +1,11 @@
 import { fetchPost } from '$lib/utils';
+import { redirect } from '@sveltejs/kit';
 
 /** @type {import("./$types").PageLoad} */
 export async function load({ params }) {
-	return await fetchPost('calendario', params.event);
+	let post = await fetchPost('calendario', params.event);
+	if (post.meta?.redirect) {
+		throw redirect(307, post.meta.link);
+	}
+	return post
 }
